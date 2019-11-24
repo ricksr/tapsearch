a68a12146ca0fb5d6e075f41e9728bab0252688e
sutirtha rej <sutirtharej@gmail.com>
2019-11-25 02:23:02 +0530

    Update upl.js

--- upl.js
+++ upl.js
@@ -1,6 +1,7 @@
 document.getElementById('input-file')
   .addEventListener('change', getFile)
 
+
 function getFile(event) {
 	const input = event.target
   if ('files' in input && input.files.length > 0) {
@@ -10,17 +11,19 @@ function getFile(event) {
   }
 }
 
+
 function placeFileContent(target, file) {
 	readFileContent(file).then(content => {
   	target.value = content
   }).catch(error => console.log(error))
 }
 
+
 function readFileContent(file) {
-	const reader = new FileReader()
-  return new Promise((resolve, reject) => {
-    reader.onload = event => resolve(event.target.result)
-    reader.onerror = error => reject(error)
-    reader.readAsText(file)
-  })
+ const reader = new FileReader()
+ return new Promise((resolve, reject) => {
+	 reader.onload = event => resolve(event.target.result)
+	 reader.onerror = error => reject(error)
+	 reader.readAsText(file)
+ })
 }
