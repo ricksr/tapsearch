65ddf747298602eb3da98be3ac81b1c1aae47d2a
sutirtha rej <sutirtharej@gmail.com>
2019-11-25 02:25:26 +0530

    Update upl.js

--- upl.js
+++ upl.js
@@ -31,3 +31,19 @@ function readFileContent(file) {
 	 reader.readAsText(file)
  })
 }
+function readFileContent(file) {
+ const reader = new FileReader()
+ return new Promise((resolve, reject) => {
+	 reader.onload = event => resolve(event.target.result)
+	 reader.onerror = error => reject(error)
+	 reader.readAsText(file)
+ })
+}
+function readFileContent(file) {
+ const reader = new FileReader()
+ return new Promise((resolve, reject) => {
+	 reader.onload = event => resolve(event.target.result)
+	 reader.onerror = error => reject(error)
+	 reader.readAsText(file)
+ })
+}
