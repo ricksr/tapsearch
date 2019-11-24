9fc5f583cc383e06a01e8790cb2c1e8fe90e5b85
ricksr <sutirtharej@gmail.com>
2019-11-24 15:11:01 +0530

    third commit

--- solve.js
+++ solve.js
@@ -1,5 +1,5 @@
 var total;
-var map1 = new Map();
+var map1;
 let str = [];
 
 function clear_all() {
@@ -7,11 +7,11 @@ function clear_all() {
   total = "";
   document.getElementById("demo").innerHTML = total;
 }
+
 function search_input() {
   total = document.getElementById("txt").value;
   total = total.toLowerCase();
-  total.replace( /,/g, "" );
-  total.replace( /./g, "" );
+  var temp = total;
   document.getElementById("demo").innerHTML = total;
   map1 = new Map();
   var set1 = new Set();
@@ -21,8 +21,7 @@ function search_input() {
       var words = para.split(" "); // words
       // i is the paragraph number
       for(var j = 0; j < words.length; j++) {
-          //map1.set(words[j], i+1);
-          //map1[words[j]].push(i+1);
+          words[j] = words[j].replace(/[&\/\\#,+()$~%.'":*?<>{}\n]/g, '');
           var key = words[j];
           map1[key] = map1[key] || [];
           map1[key].push(i+1);
@@ -38,16 +37,26 @@ function search_input() {
   }
 
 }
-
+var wrd;
 function search_output() {
-  var wrd = document.getElementById("text1").value;
-  if(total.length == 0) {
+  // wrd is word to search in top 10
+  wrd = document.getElementById("text1").value;
+  wrd = wrd.toLowerCase();
+  // no text
+  if( (total.length == 0) || (total == undefined) ) {
     document.getElementById("maps").innerHTML = "Enter Text please"
   }
-  else {
-    document.getElementById("maps").innerHTML = map1[wrd];
+  // present
+  else if(map1[wrd] != undefined) {
     for(let item of map1[wrd]) {
       alert("present in paragraph " + item);
     }
+    document.getElementById("maps").innerHTML = map1[wrd];
+  } 
+  // word not found
+  else {
+    alert("word not found, please try another");
+    document.getElementById("maps").innerHTML = "oops!! Word Not Found , Please try another !";
   }
+  
 }
